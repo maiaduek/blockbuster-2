701632a8f01653e1d0d742d8989304dddc431a78
Maia Duek <duekmaia@gmail.com>
2022-02-15 14:23:07 -0500

    integrated api, getting random movies

--- routes/movie-api.js
+++ routes/movie-api.js
@@ -2,25 +2,6 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-// router.get('/random-movie', (req, res) => {
-//   axios({
-//     method: "GET",
-//     url: 'https://movies-tvshows-data-imdb.p.rapidapi.com/',
-//     params: {type: 'get-random-movies', page: '1'},
-//     headers: {
-//       'x-rapidapi-host': 'movies-tvshows-data-imdb.p.rapidapi.com',
-//       'x-rapidapi-key': 'a460bb0bd0msh814d08d164cd76bp1b1451jsn09211e97f6aa'
-//     }
-//   })
-//   .then(results => {
-//     console.log("RANDOM MOVIE:::", results)
-//   })
-//   .catch(err => {
-//     console.log("err random movie", err)
-//   })
-// })
-
-
 router.get('/random-movie', (req, res) => {
   const options = {
     method: 'GET',
@@ -33,7 +14,8 @@ router.get('/random-movie', (req, res) => {
   };
   
   axios.request(options).then(function (response) {
-    console.log(response.data);
+    console.log(console.log("RESPONSE DATA::",response.data));
+    res.render('movies/random-movie', {movies: response.data.movie_results})
   }).catch(function (error) {
     console.error(error);
   });
